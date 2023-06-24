f25e29f30563fd312fdd1fc075a78d1c9469d3ee
RadimBaca <radim.baca@vsb.cz>
2023-06-24 12:10:06 +0200

    oracle test

--- sql/agg_test/agg_pb_ob_dropindexes_oracle.sql
+++ sql/agg_test/agg_pb_ob_dropindexes_oracle.sql
@@ -4,3 +4,4 @@ DROP INDEX idx_tab_c;DROP INDEX idx_tab_a;
 DROP INDEX idx_tab_c;DROP INDEX idx_tab_b;
 DROP INDEX idx_tab_c;DROP INDEX idx_tab_a;DROP INDEX idx_tab_b;
 DROP INDEX idx_tab_c;DROP INDEX idx_tab_a_b;
+DROP INDEX idx_tab_c;DROP INDEX idx_tab_b_a;
