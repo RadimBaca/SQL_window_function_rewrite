6d3fc2e47d9e94a3f92dc3633cab9906021be0ad
RadimBaca <radim.baca@vsb.cz>
2024-03-04 09:57:26 +0100

    Visual points in graphs changed

--- result_analysis/rank_bdistinct.py
+++ result_analysis/rank_bdistinct.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from result_analysis.manipulation import read_data, compute_means
 
+
 counter = 0
 display_dbms_text = {
     'Hyper': 'Hyper',
@@ -17,53 +18,50 @@ display_dbms_text = {
     'MSSql': 'DBMS1',
     'Oracle': 'DBMS2'
 }
-display_bdistinct = {
-    10: '0.001',
-    30000: '3'
+
+display_algorithm_name = {
+    'JoinMin' : 'JoinMin',
+    'LateralAgg' : 'LateralAgg',
+    'LateralLimit' : 'LateralLimitTies',
+    'LateralDistinctLimit' : 'LateralDistinctLimitTies',
 }
 display_point = {
-    10: 'o',
-    30000: 'x'
+    'JoinMin' : 'o',
+    'LateralDistinctLimit' : 'x'
 }
 
-def agg_analysis(dbms_list, index, print_legend):
+def rank_bdistinct_analysis(dbms_list, has_cost, storage, index, print_legend):
     global counter, file, lines, columns, pattern, line, data, column_data, row_data, parallel_on, parallel_off, padding_on, padding_off, equal1_data, equalN_data, lessN_data, IB_data, Not_IB_data, IA_data, Not_IA_data, IBA_data, IAB_data, X
 
     data = {}
 
     for (dbms, alg, color) in dbms_list:
         # Read the CSV file into a DataFrame, reading each row as a string
-        with open('agg_' + dbms + '_largesel.txt', 'r') as file:
+        with open('rank_algorithms_' + dbms + '.txt', 'r') as file:
             lines = file.readlines()
-
-        has_cost = ('query_cost' in lines[0]) # check if the first line contains 'query_cost' substring
-        columns = ['Fun', 'Constructs', 'Sel']
+        columns = ['Cmp']
         data[dbms] = read_data(has_cost, lines, columns)
-        if dbms != 'Hyper':
-            data[dbms] = data[dbms][data[dbms]['Storage'] == "ROW"]
+        # data[dbms] = data[dbms][data[dbms]['Storage'] == storage]
 
 
     plt.rcParams['font.size'] = 14
 
-
-    for (dbms,bd, color) in dbms_list:
+    for (dbms,alg, color) in dbms_list:
         selected_data = data[dbms]
         filtered_selected_data = selected_data[
             (selected_data['Par'] == 'parallel_ON') &
             (selected_data['Padding'] == 'padding_OFF') &
             (selected_data['IDX'] == index) &
-            (selected_data['Fun'] == 'min') &
-            (selected_data['Constructs'] == 'PB_OB') &
-            (selected_data['PB'] == bd)
+            (selected_data['Alg'] == alg)
             ]
 
         # pb_values = [10, 30, 100, 300, 1000, 3000, 10000, 30000]
-        sel_values = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048]
+        pb_values = [0.001, 0.003, 0.01, 0.03, 0.1, 0.3, 1, 3]
         division_result_list = (filtered_selected_data['T1'] / filtered_selected_data['T2']).tolist()
-        plt.plot(sel_values, division_result_list, marker=display_point[bd], label=display_dbms_text[dbms] + ', bdistinct: ' + display_bdistinct[bd], color=color)
+        plt.plot(pb_values, division_result_list, marker=display_point[alg], label=display_dbms_text[dbms] + ' ' + display_algorithm_name[alg], color=color)
 
     plt.axhline(y=1, color='red', linestyle='--')
-    plt.ylim(0.001, 1000)
+    plt.ylim(0.0001, 20)
     plt.xscale('log')
     plt.yscale('log')
     if print_legend:
@@ -71,36 +69,46 @@ def agg_analysis(dbms_list, index, print_legend):
         plt.ylabel(r'$T_{lin}\,/\,T_{sj}$', fontsize=20)
 
     # Label axes and add title
-    plt.xlabel('Selectivity', fontsize=18)
+    plt.xlabel('BDistinct [%]', fontsize=18)
     if index == " ":
         plt.title('No index', fontsize=24)
     else:
         plt.title(index + ' index', fontsize=24)
 
+
     # plt.subplots_adjust(left=0.15, right=0.97, top=0.93, bottom=0.12)
-    plt.tight_layout()
-    plt.savefig('agg_sel_' + str(counter) + '.pdf', format='pdf')
     counter += 1
+    # Show the plot
+    plt.tight_layout()
+    plt.savefig('rank_bdistinct_' + str(counter) + '.pdf', format='pdf')
     plt.show()
 
 def without_index():
     global dbms_list, alg, result
     colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#1f77b4', '#ff7f0e', '#2ca02c', '#d62728']
-    dbms_list = ['MSSql', 'Postgres', 'Oracle', 'Hyper']
-    bdistinct = [10, 30000]
-    result = [(d, bd) for bd in bdistinct for d in dbms_list]
+    # colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f']
+    dbms_list = ['MSSql', 'Postgres', 'Oracle', "Hyper"]
+    # alg = ['LateralAgg', 'LateralLimit']
+    alg = ['JoinMin', 'LateralDistinctLimit']
+    result = [(d, a) for a in alg for d in dbms_list]
     result = [(val1, val2, color) for color, (val1, val2) in zip(colors, result)]
-    agg_analysis(result, " ", True)
+
+    rank_bdistinct_analysis(result, False, "ROW", " ", True)
 
 def with_index():
     global dbms_list, alg, result
+    # colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#9467bd', '#8c564b', '#e377c2']
     colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#1f77b4', '#ff7f0e', '#2ca02c',]
     dbms_list = ['MSSql', 'Postgres', 'Oracle']
-    bdistinct = [10, 30000]
-    result = [(d, bd) for bd in bdistinct for d in dbms_list]
+    # alg = ['LateralAgg', 'LateralLimit']
+    alg = ['JoinMin', 'LateralDistinctLimit']
+    result = [(d, a) for a in alg for d in dbms_list]
     result = [(val1, val2, color) for color, (val1, val2) in zip(colors, result)]
-    agg_analysis(result, "I(A);I(B)", False)
-    agg_analysis(result, "I(BA)", False)
+    # rank_bdistinct_analysis(result, False, "ROW", "I(B)", False)
+    # rank_bdistinct_analysis(result, False, "ROW", "I(A)", False)
+    rank_bdistinct_analysis(result, False, "ROW", "I(A);I(B)", False)
+    # rank_bdistinct_analysis(result, False, "ROW", "I(AB)", False)
+    rank_bdistinct_analysis(result, False, "ROW", "I(BA)", False)
 
 without_index()
 with_index()
